2f87f486b2b0165ec9b98f40e99064c109991dc5
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-01-29 18:09:35 +0600

    box created

---
+++ src/assets/Square.jsx
@@ -0,0 +1,16 @@
+
+const Square = ({value}) => {
+    const handleClick = () =>{
+        console.log('clicked!')
+    }
+  return (
+    <>
+        <button 
+        className="square"
+        onClick={handleClick}
+        >{value}</button>
+    </>
+  )
+}
+
+export default Square
